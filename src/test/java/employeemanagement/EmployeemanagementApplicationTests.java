1448aa885ce85ad9c4aec891663565ef6fa94967
Shankar Ghimire <ssghimire@gmail.com>
2020-10-03 13:23:12 -0400

    Initializes Spring Boot Project

---
+++ src/test/java/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
